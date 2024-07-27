144e49af6b6935c3b9496123b5502f9b3848b0a2
KrishnaPrasad05 <krishnasrinivasan41@gmail.com>
2024-07-27 13:52:16 +0530

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/recipe-writings/build', 'index.html'));
 });
 
+console.log('MongoDB URI:', process.env.MONGO_URI);
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
